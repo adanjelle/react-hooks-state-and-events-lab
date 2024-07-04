e16c0e9d6c19ae59efcd74a4bf6399a5fd34cd37
adanjelle <adanjelle4@gmail.com>
2024-07-04 08:43:48 +0300

    complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 
 function Item({ name, category }) {
-  // Step 1: Create a state variable for whether the item is in the cart
+  
   const [inCart, setInCart] = useState(false);
 
-  // Step 2: Determine the class name based on the state
+
   const itemClass = inCart ? "in-cart" : "";
 
-  // Step 3: Determine the button text based on the state
   const buttonText = inCart ? "Remove from Cart" : "Add to Cart";
 
-  // Step 4: Add an event handler to toggle the item's state
+  
   const handleCartToggle = () => {
     setInCart((prevInCart) => !prevInCart);
   };
@@ -19,7 +18,7 @@ function Item({ name, category }) {
     <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      {/* Attach the event handler to the button */}
+     
       <button className="add" onClick={handleCartToggle}>{buttonText}</button>
     </li>
   );
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,26 +2,19 @@ import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
-  // Step 1: Create a state variable for the selected category
   const [selectedCategory, setSelectedCategory] = useState("All");
-
-  // Step 2: Update state when the value of the <select> element changes
   const handleCategoryChange = (event) => {
     setSelectedCategory(event.target.value);
   };
-
-  // Step 3: Filter the items based on the selected category
   const filteredItems = items.filter((item) => {
     if (selectedCategory === "All") {
       return true;
     }
     return item.category === selectedCategory;
   });
-
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        {/* Attach the event handler to the <select> element */}
         <select name="filter" value={selectedCategory} onChange={handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
